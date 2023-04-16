ba17ad0fd209e6b27dced6332309d08fd7756f6d
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:19:03 +0800

    Update abstract_public_holiday.sh

--- src/abstract_public_holiday.sh
+++ src/abstract_public_holiday.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 api="https://holidays.abstractapi.com/v1"
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
 function set_api_key() {
 	# 1 - api_key: (string): <YOUR_UNIQUE_API_KEY>
@@ -14,6 +15,6 @@ function get_holidays() {
 	# 4 - day: (integer): <day>
 	curl --request GET \
 		--url "$api?api_key=$api_key&country=${1}&year=${2:-$(date +%Y)}&month=${3:-0}&day=${4:-0}" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/json"
 }
